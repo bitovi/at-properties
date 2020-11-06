7875236553e7a088228dbca656c0538c6708c0c1
Nick Charbonneau <nick@bitovi.com>
2020-11-05 17:51:33 -0800

    Title Card H1

--- src/containers/title.container.vue
+++ src/containers/title.container.vue
@@ -1,9 +1,10 @@
 <template>
     <header class='dlp-page dlp-hero justify-between' :style="`background-image: url('${publicPath}images/bg/bg-agent.jpg')`">
         <img class="mt-4 md:mt-12 w-2/4 md:w-auto mb-4" alt="@properties" :src="`${publicPath}images/logos/logo-atproperties.svg`">
-        <div class="container mx-auto flex flex-col">
+        <div class="container mx-auto flex flex-col text-center">
             <img class="pb-8 sm:pb-20" alt="Stop Looking. Start Finding." :src="`${publicPath}images/intro/stop-looking.svg`">
-            <h1 class="presentation-heading">Presented by Thad Wong for Stephanie Huarte & Mekael Wesley-Rosa</h1>
+            <h1 class="title-heading">Top 1% in Chicago<br>$250M Sales</h1>
+            <p class="label-heading">Presented by Thad Wong for Stephanie Huarte & Mekael Wesley-Rosa</p>
         </div>
         <scroll-notify class=""></scroll-notify>
     </header>
