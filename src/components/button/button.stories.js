61775687e57badb3929ff1da83868ac6f34a0d8a
Nick Charbonneau <nick@bitovi.com>
2020-11-09 12:55:45 -0800

    Button story

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,19 +1,17 @@
 import DLPButton from './button.component.vue';
 
 export default {
-  title: 'DLP/Button',
-  component: DLPButton,
-  argTypes: {
-  },
+    title: 'DLP/Button',
+    component: DLPButton,
+    argTypes: {
+        nativeType: { control: { type: 'select', options: [null, 'button', 'submit', 'reset'] } }
+    },
 };
 
 const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { DLPButton },
-  template: '<dlp-button type="button">Test Button</dlp-button>',
+    props: Object.keys(argTypes),
+    components: { DLPButton },
+    template: '<dlp-button v-bind="$props">Test Button</dlp-button>',
 });
 
 export const Primary = Template.bind({});
-Primary.args = {
-    
-};
