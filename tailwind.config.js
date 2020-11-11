fb65460a3abc641ce0925e7bd48aa4f7cf2e9636
Nick Charbonneau <nick@bitovi.com>
2020-11-11 11:11:38 -0800

    Update breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
    screens: {
       sm: '640px',
       md: '768px',
-      lg: '1024px',
-      xl: '1280px',
+      lg: '1280px',
+      xl: '1920px',
     },
     colors: {
       transparent: 'transparent',
