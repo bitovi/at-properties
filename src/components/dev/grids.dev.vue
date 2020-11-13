16765a38467bd91ef9bbd909579ee6650ba2f100
Nick Charbonneau <nick@bitovi.com>
2020-11-13 09:53:18 -0800

    Fix head-6. Font changer

--- src/components/dev/grids.dev.vue
+++ src/components/dev/grids.dev.vue
@@ -21,17 +21,29 @@
     </div>
 </template>
 <script>
+const ElBody = document.getElementsByTagName("body")[0]
+
 export default {
     name: 'dev-grids',
     data() {
         return {
-            showEl: false
+            showEl: false,
+            bookFont: false
         }
     },
     mounted: function () {
+        //g
         document.addEventListener("keydown", (event) => {
             if((event.ctrlKey || event.metaKey) && event.altKey && event.which == 71) {
                 this.toggleGrid()
+            }
+        })  
+
+
+        //t
+        document.addEventListener("keydown", (event) => {
+            if((event.ctrlKey || event.metaKey) && event.altKey && event.which == 84) {
+                this.toggleBookFont()
             }
         })  
     },
@@ -47,6 +59,17 @@ export default {
                 this.hideGrid()
             } else {
                 this.showGrid()
+            }
+        },
+        toggleBookFont() {
+            if(this.bookFont){
+                ElBody.classList.remove('font-book')
+                this.bookFont = false
+                console.log('Font: Gotham Medium')
+            } else {
+                ElBody.classList.add('font-book')
+                this.bookFont = true
+                console.log('Font: Gotham Book')
             }
         }
     }
