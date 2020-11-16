cf35c48c8df0ab592eb8b25cd5630b842f685bff
Michael H <69012635+itsmikeh@users.noreply.github.com>
2020-11-16 11:54:46 -0800

    Move animation to title component

--- src/App.vue
+++ src/App.vue
@@ -49,9 +49,6 @@
 </template>
 
 <script>
-import gsap from 'gsap';
-import ScrollTrigger from "gsap/ScrollTrigger";
-
 import Nav from './components/nav/nav.component.vue'
 import TitleContainer from './containers/title.container.vue'
 import AgentContainer from './containers/agent.container.vue'
@@ -88,48 +85,6 @@ export default {
       return {
           publicPath: process.env.BASE_URL
       }
-  },
-  mounted() {
-    gsap.registerPlugin(ScrollTrigger);
-    // gsap.from("#company .bg", {
-    //     // yPercent: 50,
-    //     // ease: "none",
-    //     // duration: 5,
-    //     transform: 'perspective(100px) translate3d(0, 0, 0px)',
-    //     opacity: 1,
-    //     // x: 300,
-    //     // onUpdate() {
-    //     //   // also onStart, onComplete
-    //     //   // console.log('update');
-    //     // },
-    //     scrollTrigger: {
-    //         trigger: "#company hgroup",
-    //     //     // start: "top bottom", // the default values
-    //         end: "bottom top",
-    //         scrub: true
-    //     },
-    // });
-    const elSelector = '#company hgroup';
-    const el = document.querySelector(elSelector);
-    const elOffsetH = el.offsetHeight;
-
-    gsap.from(el, {
-      ease: 'sine-inOut',
-      duration: 1.5,
-      transform: 'perspective(100px) translate3d(0, 300px, 0)',
-      opacity: 0,
-      scrollTrigger: {
-        trigger: el,
-        start() {
-          return `top bottom-=${elOffsetH}px`
-        }, // the default values trigger scroller
-        end() {
-          return `50%-${elOffsetH} 50%`
-        }, // triggger scroller
-        scrub: true,
-        markers: true,
-      },
-    });
   },
 }
 </script>
--- src/components/title/title.page.vue
+++ src/components/title/title.page.vue
@@ -1,8 +1,8 @@
 <template>
     <header v-bind="$attrs" class='dlp-page dlp-hero justify-center' style="position:relative; overflow: hidden;">
-        <div class="bg" :style="`background-image: url('${backgroundUrl}'); background-repeat: no-repeat; background-size: cover; position:absolute; height:100%; width:100%; z-index: 0; transform: perspective(100px) translate3d(0, 0, 30px); opacity: 0.3; will-change: transform, opacity;`">
+        <div ref="backgroundImage" class="bg" :style="`background-image: url('${backgroundUrl}'); background-repeat: no-repeat; background-size: cover; position:absolute; height:100%; width:100%; z-index: 0; transform: perspective(100px) translate3d(0, 0, 30px); opacity: 0.3; will-change: transform, opacity;`">
         </div>
-        <hgroup class="container mx-auto" style="z-index: 1; transform: perspective(100px) translate3d(0, 0px, 0); opacity: 1; will-change: transform, opacity;">
+        <hgroup ref="titleText" class="container mx-auto" style="z-index: 1; transform: perspective(100px) translate3d(0, 0px, 0); opacity: 1; will-change: transform, opacity;">
             <h1 class="head-1 text-center accent accent-wide accent-centered accent-below">{{heading}}</h1>
             <h2 class="head-6 text-center" v-if="subheading">{{subheading}}</h2>
         </hgroup>
@@ -10,6 +10,13 @@
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+import gsap from 'gsap';
+// eslint-disable-next-line no-unused-vars
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default {
     name: 'dlp-title-page',
     props: {
@@ -26,5 +33,44 @@ export default {
             required: false
         }
     },
+    mounted() {
+        const textElement = this.$refs.titleText;
+        const textHeight = textElement.offsetHeight;
+
+        const imageElement = this.$refs.backgroundImage;
+        // eslint-disable-next-line no-unused-vars
+        const imageHeight = imageElement.offsetHeight;
+
+        gsap.from(textElement, {
+            ease: 'sine-inOut',
+            duration: 1.5,
+            transform: 'perspective(100px) translate3d(0, 300px, 0)',
+            opacity: 0,
+            scrollTrigger: {
+                trigger: textElement,
+                start() {
+                    return `top bottom-=${textHeight}px`
+                },
+                end() {
+                    return `50%-${textHeight} 50%`
+                },
+                scrub: true,
+                markers: true,
+            },
+        });
+
+        gsap.from(imageElement, {
+            ease: 'sine-inOut',
+            duration: 1.5,
+            transform: 'perspective(100px) translate3d(0, 0, 0px)',
+            scrollTrigger: {
+                trigger: textElement,
+                start: "top bottom",
+                end: "bottom top",
+                scrub: true,
+                markers: true,
+            },
+        });
+    }
 }
 </script>
