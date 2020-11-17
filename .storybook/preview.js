f28b577ae5a5eaad1f4f07cfe5e82943ad661e6a
Hanna Kutcher <hkutcher@Hannas-MacBook-Pro.local>
2020-11-17 17:40:48 -0500

    Changed order of items in sidebar

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,4 +7,9 @@ Vue.use(VueScrollactive);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: {
+      order: ['Foundation', ['Platform Scale', 'Color', 'Typography', 'Object Styles', 'Motion', 'Iconography', 'Imagery', 'Layout', 'Inclusive Design'], 'Components', 'Example']
+    }
+  }
 }
