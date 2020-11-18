c6c0469a6916063fc26035f0a067e0e3c199cbd3
Nick Charbonneau <nick@bitovi.com>
2020-11-18 14:58:26 -0800

    Remove Title h1

--- src/containers/title.container.vue
+++ src/containers/title.container.vue
@@ -2,9 +2,8 @@
     <header class='dlp-page dlp-hero justify-between' :style="`background-image: url('${publicPath}images/bg/bg-agent.jpg')`">
         <img class="mt-4 md:mt-12 w-2/4 md:w-auto mb-4" alt="@properties" :src="`${publicPath}images/intro/logo-atproperties.svg`">
         <div class="container mx-auto flex flex-col text-center">
-            <img class="pb-8 sm:pb-20 md:pb-6 w-2/4 mx-auto" alt="Stop Looking. Start Finding." :src="`${publicPath}images/intro/stop-looking.svg`">
-            <h1 class="head-1">Top 1% in Chicago<br>$250M Sales</h1>
-            <p class="head-6">Presented by Thad Wong for Stephanie Huarte & Mekael Wesley-Rosa</p>
+            <img class="pb-8 sm:pb-20 w-2/4 mx-auto" alt="Stop Looking. Start Finding." :src="`${publicPath}images/intro/stop-looking.svg`">
+            <h1 class="head-6">Presented by Thad Wong for Stephanie Huarte & Mekael Wesley-Rosa</h1>
         </div>
         <scroll-notify />
     </header>
