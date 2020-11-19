7e2b1f7bbc10cd493052cbea03452000e1965ee9
Nick Charbonneau <nick@bitovi.com>
2020-11-19 10:55:09 -0800

    Team content updates

--- src/containers/team.container.vue
+++ src/containers/team.container.vue
@@ -17,13 +17,13 @@
                         class="sm:mx-auto my-8 md:mb-8 md:my-0 md:mx-0"
                         alt="Olivia Carlson" 
                         :src="`${publicPath}images/team/logo-olivia-carson.jpg`">
-                    <h1 class="head-1">Olivia Carlson</h1>
+                    <h1 class="head-1">i18n.name</h1>
                     <h2 class="head-5 accent">Team Bio</h2>
-                    <p v-html="i18n['olivia-arson'].sample"></p>
+                    <p v-html="i18n.bio.brief"></p>
                     <div class="btn-group">
                         <dlp-button type="button" @click="showModal('AgentModal')">Read Full Bio</dlp-button>
                         <dlp-slide-in ref="AgentModal">
-                            <div v-html="i18n['olivia-arson'].full"></div>
+                            <div v-html="i18n.bio.full"></div>
                         </dlp-slide-in>
                         <dlp-button type="button">Watch Team Video</dlp-button>
                     </div>
@@ -34,16 +34,16 @@
         <section id="team-stats" class="my-16">
             <div class="grid md:grid-cols-3 border-t border-b border-primary-100 divide-y md:divide-y-0 md:divide-x divide-primary-100">
                 <div class="p-8 text-center stat">
-                    <span class="head-3 block callout-text" aria-label="300 plus">300+</span> 
-                    <span class="head-6">transactions</span>
+                    <span class="head-3 block callout-text" v-html="i18n.stats[0]['span-1']"></span> 
+                    <span class="head-6" v-html="i18n.stats[0]['span-2']"></span>
                 </div>
                 <div class="p-8 text-center stat">
-                    <span class="head-3 block callout-text" >10 years</span> 
-                    <span class="head-6">selling real estate</span>
+                    <span class="head-3 block callout-text" v-html="i18n.stats[1]['span-1']"></span> 
+                    <span class="head-6" v-html="i18n.stats[1]['span-2']"></span>
                 </div>
                 <div class="p-8 text-center stat">
-                    <span class="head-3 block callout-text" aria-label="Over 24 million dollars">$24M+</span> 
-                    <span class="head-6">total sales</span>
+                    <span class="head-3 block callout-text" v-html="i18n.stats[2]['span-1']"></span> 
+                    <span class="head-6" v-html="i18n.stats[2]['span-2']"></span>
                 </div>
             </div>
         </section>
@@ -55,7 +55,7 @@
     </div>
 </template>
 <script>
-import BioStrings from '../assets/strings/bio.i18n.json'
+import BioStrings from '../assets/strings/team.i18n.json'
 
 export default {
     name: 'teamContainer',
