632ea5e2a0468365342b9259fa8e1477593e5ced
Hanna Kutcher <hkutcher@Hannas-MacBook-Pro.local>
2020-11-19 12:17:13 -0500

    Styled storybook, added change log page.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import '../src/components/index'
 import '../src/assets/base.styles.scss'
 
+import { themes } from '@storybook/theming';
+
 import VueScrollactive from 'vue-scrollactive';
 Vue.use(VueScrollactive);
 
@@ -9,7 +11,10 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   options: {
     storySort: {
-      order: ['Welcome', 'Foundation', ['Front-End Stack', 'Design System', 'Design Tokens', 'Breakpoints', 'Color', 'Typography', 'Object Styles', 'Iconography', 'Imagery', 'Layout', 'Motion'], 'Components', ['Button', 'Figure', 'Heading', 'Main Navigation', 'Modal', 'Paragraph', 'Property', 'Section', 'Social', 'Statistic', 'Testimonial', 'Video']]
+      order: ['Welcome', 'Change Log', 'Foundation', ['Design Tokens', 'Breakpoints', 'Color', 'Typography', 'Object Styles', 'Iconography', 'Imagery', 'Layout', 'Motion'], 'Components', ['Button', 'Figure', 'Heading', 'Main Navigation', 'Modal', 'Paragraph', 'Property', 'Section', 'Social', 'Statistic', 'Testimonial', 'Video']]
     }
+  },
+  docs: {
+    theme: themes.light,
   }
 }
