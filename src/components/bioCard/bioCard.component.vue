3ecf2ad44d3f9693183187597aea162f2b1a4f6c
Nick Charbonneau <nick@bitovi.com>
2020-11-20 10:07:40 -0800

    Framework

---
+++ src/components/bioCard/bioCard.component.vue
@@ -0,0 +1,28 @@
+<template>
+    <figure>
+        <img srch="images/company/bio-1.jpg" alt=""/>
+        <figcaption>
+            <span></span>
+            <span class="accent accent-centered"></span>
+        </figcaption>
+    </figure>
+</template>
+<script>
+export default {
+    name: 'dlp-bio-card',
+    props: {
+        source: {
+            type: String,
+            default: null
+        },
+        name: {
+            type: String,
+            default: null
+        },
+        caption: {
+            type: String,
+            default: null
+        },
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueScrollactive from 'vue-scrollactive';
+import VueCarousel from 'vue-carousel';
 import VueYoutube from 'vue-youtube'
 
 import './components/index'
 
 Vue.use(VueScrollactive);
+Vue.use(VueCarousel);
 Vue.use(VueYoutube);
 
 Vue.config.productionTip = false
