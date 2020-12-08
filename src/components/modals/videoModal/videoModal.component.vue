0aebe9c67bcdde5bcb82e2d49dd6f2537b1447f9
Nick Charbonneau <nick@bitovi.com>
2020-12-08 14:08:38 -0800

    wiring background on video modal

--- src/components/modals/videoModal/videoModal.component.vue
+++ src/components/modals/videoModal/videoModal.component.vue
@@ -1,50 +1,40 @@
-// Possible solution to showing ads
-// It's a bit trickstery 
-// https://support.google.com/policies/answer/9664901
 
-// also
-// https://www.geekytech.co.uk/remove-disable-related-videos-youtube-embed/
-
-// Known Dev Bug:
-// The target origin provided ('https://www.youtube.com') does not match the recipient window's origin ('http://localhost:8081').
-// When in production with https, this should disapear
 <template>
-    <transition
-        name="fade"
-        v-on:after-enter="setFocus"
-        >
-        <div 
-            class="dlp-video-modal-mask" 
-            @click.self="close" 
-            v-if="showModal"
-            role="dialog"
-            aria-modal="true"
-            aria-describedby="modalDescription">
-                <div class="dlp-video-modal content-grid">
-                    <nav class="absolute right-0 mr-4 lg:mr-12 lg:mt-12">
-                        <h1 
-                            ref="ElDescription"
-                            tabindex="0" 
-                            id="modalDescription"
-                            class="visually-hidden">
-                            {{srHeading}}
-                        </h1>
-                        <dlp-close-btn @click.stop="close" :light="true" />
-                    </nav>
-                    <div class="dlp-video-modal-content col-span-4 md:col-span-12 lg:col-span-12 lg:col-start-3" >
-                        <div class="dlp-video-border">
-                            <div class="dlp-video-modal-player">
-                                <youtube :fitParent="true" :video-id="videoId" :player-vars="playerVars"/>
-                            </div>
+    <div 
+        class="dlp-video-modal-mask" 
+        @click.self="close" 
+        v-if="showModal"
+        role="dialog"
+        aria-modal="true"
+        aria-describedby="modalDescription">
+            <dlp-bg-ripple ref="dlpBg" @openDone="showContent" @closeDone="cleanup"/>
+            <div class="dlp-video-modal content-grid" ref="dlpContent">
+                <nav class="absolute right-0 mr-4 lg:mr-12 lg:mt-12">
+                    <h1 
+                        ref="ElDescription"
+                        tabindex="0" 
+                        id="modalDescription"
+                        class="visually-hidden">
+                        {{srHeading}}
+                    </h1>
+                    <dlp-close-btn @click.stop="close" :light="true" />
+                </nav>
+                <div class="dlp-video-modal-content col-span-4 md:col-span-12 lg:col-span-12 lg:col-start-3" >
+                    <div class="dlp-video-border">
+                        <div class="dlp-video-modal-player">
+                            <youtube :fitParent="true" :video-id="videoId" :player-vars="playerVars"/>
                         </div>
-                        <span v-if="caption" class="head-6 accent--above-center text-white pt-4">{{caption}}</span>
                     </div>
+                    <span v-if="caption" class="head-6 accent--above-center text-white pt-4">{{caption}}</span>
                 </div>
-        </div>
-    </transition>
+            </div>
+    </div>
 </template>
 <script>
-import A11yStrings from '../../../assets/strings/a11y.i18n.json'
+import gsap from 'gsap'
+import Background from '../util/background.component.vue'
+import '../util/background.styles.scss'
+import { hasMotion } from '../../../constants'
 // ref to <body>
 const ElBody = document.getElementsByTagName("body")[0]
 
@@ -53,7 +43,7 @@ export default {
     props: {
         srHeading:{
             type: String,
-            default: A11yStrings.defaults.videoModal.heading
+            default: "Beginning of dialog window. It does not collect any information. Escape will close the window."
         },
         videoId: {
             type: String,
@@ -70,8 +60,12 @@ export default {
             playerVars: {
                 rel: 0,
                 autoplay: 0
-            }
+            },
+            openerEl: null
         }
+    },
+    components: {
+        [Background.name]: Background
     },
     mounted: function () {
         document.addEventListener("keydown", (e) => {
@@ -81,16 +75,36 @@ export default {
         })  
     },
     methods: {
-        open() {
+        open(evt) {
+            this.showModal = true
+            this.openerEl = evt
+
+            if(hasMotion){
+                gsap.set(this.$refs.dlpContent, { opacity: 0})
+            }
+            
+            this.$nextTick(()=> {
+                this.$refs.dlpBg.open(this.openerEl)
+            })
+            
             try {
                 ElBody.classList.add('lock-scroll')
             } catch (error) {
                 console.warn('Unable to fix scroll positon')   
             }
-            
-            this.showModal = true
         },
-        close: function() {
+        showContent() {
+            this.$nextTick(()=> {
+                console.log(this.$refs.dlpContent)
+                if(hasMotion){
+                    gsap.to(this.$refs.dlpContent, { 
+                        opacity: 1
+                    })
+                }
+            })
+            
+        },
+        cleanup() {
             try {
                 ElBody.classList.remove('lock-scroll')
             } catch (error) {
@@ -98,6 +112,9 @@ export default {
             }
             this.showModal = false
             this.$emit('close')
+        },
+        close: function() {
+            this.$refs.dlpBg.close(this.openerEl)
         },
         setFocus: function() {
             this.$refs.ElDescription.focus({preventScroll: true})
