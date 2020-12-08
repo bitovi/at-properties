e0031dcc5c7ce80e761d7e9c4cd2fc2d80a3f642
Nick Charbonneau <nick@bitovi.com>
2020-12-08 11:38:15 -0800

    Add reach modal

--- src/containers/reach.container.vue
+++ src/containers/reach.container.vue
@@ -14,7 +14,7 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque gravida ac, diam malesuada. Fringilla arcu fusce tempus sodales velit id. Tristique sed nec dignissim imperdiet. Id eu cursus et parturient suscipit.</p>
                 </dlp-transition>
                 <div class="btn-group pb-16">
-                    <dlp-button type="button" @click="showModal('ReachVideo')">Our Global Reach</dlp-button>
+                    <dlp-button type="button" @click="showModal('ReachVideo', $event)">Our Global Reach</dlp-button>
                     <dlp-video-modal ref="ReachVideo" videoId="DgZb_NRk-Bk" />
                 </div>
             </div>
@@ -33,7 +33,12 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque gravida ac, diam malesuada. Fringilla arcu fusce tempus sodales velit id. Tristique sed nec dignissim imperdiet. Id eu cursus et parturient suscipit.</p>
                 </dlp-transition>
                 <div class="btn-group">
-                    <dlp-button type="button">View Luxury Stats</dlp-button>
+                    <dlp-button type="button" @click="showModal('LuxaryStatsModal', $event)">View Luxury Stats</dlp-button>
+                    <dlp-appear-modal ref="LuxaryStatsModal">
+                        <h3 class="head-5 accent--above-left mb-8">Luxury Stats</h3>
+                        <p>Coming soon...</p>
+                    </dlp-appear-modal>
+
                 </div>
             </div>
         </div>
@@ -49,9 +54,9 @@ export default {
         }
     },
     methods: {
-        showModal: function(name) {
+        showModal: function(name, evt) {
             if(this.$refs[name]){
-                this.$refs[name].open()
+                this.$refs[name].open(evt)
             }
         }
     }
