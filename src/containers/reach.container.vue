324eef7be279f379e9301a63d996d0002a6bc51c
Nick Charbonneau <nick@bitovi.com>
2020-12-08 11:58:30 -0800

    Add Reach modal

--- src/containers/reach.container.vue
+++ src/containers/reach.container.vue
@@ -1,33 +1,35 @@
 <template>
     <div class='section-content'>
-        <div class="content-grid">
-            <div class="col-span-4 md:col-span-6 md:col-start-1 lg:col-span-7 lg:col-start-2">
+        <section class="content-grid">
+            <div class="col-span-full md:col-span-6 lg:col-span-7 lg:col-start-2">
                 <dlp-figure
                     :sm="`${publicPath}images/reach/reach-1.jpeg`"
-                    alt="iPhone 11 Pro"
+                    alt=""
+                    ar="ar ar--4-3"
                     animate="shrink"
                 />
             </div>
-            <div class="col-span-4 md:col-span-6 md:col-start-7 lg:col-span-6 lg:col-start-10">
+            <div class="col-span-full md:col-span-6 md:col-start-7 lg:col-span-6 lg:col-start-10 self-center">
                 <h3 class="head-5 accent--above-left">Leading RE</h3>
                 <dlp-transition type="fadeIn slideUp">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque gravida ac, diam malesuada. Fringilla arcu fusce tempus sodales velit id. Tristique sed nec dignissim imperdiet. Id eu cursus et parturient suscipit.</p>
                 </dlp-transition>
-                <div class="btn-group pb-16">
+                <div class="btn-group">
                     <dlp-button type="button" @click="showModal('ReachVideo', $event)">Our Global Reach</dlp-button>
                     <dlp-video-modal ref="ReachVideo" videoId="DgZb_NRk-Bk" />
                 </div>
             </div>
-        </div>
+        </section>
         <div class="content-grid">
             <div class="order-3 md:order-4 col-span-4 md:col-span-6 md:col-start-7 lg:col-span-7 lg:col-start-9">
                 <dlp-figure
                     :sm="`${publicPath}images/reach/reach-2.jpeg`"
-                    alt="iPhone 11 Pro"
+                    alt=""
+                    ar="ar ar--4-3"
                     animate="shrink"
                 />
             </div>
-            <div class="order-4 md:order-3 col-span-4 md:col-span-6 md:col-start-1 lg:col-span-6 lg:col-start-2">
+            <div class="order-4 md:order-3 col-span-4 md:col-span-6 md:col-start-1 lg:col-span-6 lg:col-start-2 content-center self-center">
                 <h3 class="head-5 accent--above-left">Luxury Portfolio</h3>
                 <dlp-transition type="fadeIn slideUp">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque gravida ac, diam malesuada. Fringilla arcu fusce tempus sodales velit id. Tristique sed nec dignissim imperdiet. Id eu cursus et parturient suscipit.</p>
@@ -38,7 +40,6 @@
                         <h3 class="head-5 accent--above-left mb-8">Luxury Stats</h3>
                         <p>Coming soon...</p>
                     </dlp-appear-modal>
-
                 </div>
             </div>
         </div>
